d06f93338185901b3c8d2a5a529ee85ff2f9e8e6
Jie Ni <nijie321@outlook.com>
2021-03-10 23:07:21 -0600

    code clean up

--- src/main.rs
+++ src/main.rs
@@ -10,10 +10,16 @@ mod convert;
 mod info;
 mod utility;
 
+#[derive(Debug)]
+struct Parser {
+    symbol_stack: Vec<String>,
+    content_stack: Vec<String>,
+}
 
 fn parse_markdown_file(_filename: &str){
-    // print_short_banner();
+
     println!("[ INFO ] Trying to parse {}...", _filename);
+
     let input_filename = Path::new(_filename);
     
     let mut output_filename = String::from(_filename.split('.').nth(0).unwrap());
@@ -26,99 +32,6 @@ fn parse_markdown_file(_filename: &str){
                 .expect("[ ERROR ] Failed to open file!");
 
     let mut tokens: Vec<String> = Vec::new();
-
-    let reader = BufReader::new(file);
-    
-    let mut keywords: Vec<String> = (1..=6).map(|n| "#".repeat(n) ).collect();
-        
-    
-    for line in reader.lines().map(|l| l.unwrap()){
-    // for line in reader.lines()?{
-       // let line_contents = line.unwrap();
-    
-        let contents1: Option<(&str, &str)> = line.split_once(' ');
-
-        let mut blockquote = false;
-
-        if let Some((symbol, contents)) = contents1{
-            
-            if keywords.contains(&symbol.to_string()) {
-                let s_len = symbol.len();
-                
-                let mut temp_s = format!("<h{}>", s_len);
-                
-                // blockquote
-                if &contents[0..1] == ">"{
-                    blockquote = true;
-                    temp_s.push_str("<blockquote>");
-                }
-                
-                let mut tmp_contents = &contents[..];
-                
-                utility::print_type_of(&tmp_contents);
-
-                if blockquote{
-                    tmp_contents = &tmp_contents[2..];
-                }
-
-                // println!("blockquote!!!!!!!{}",
-                temp_s.push_str(
-                    &convert::converter(&tmp_contents)
-                );
-
-                if blockquote{
-                    temp_s.push_str("</blockquote>");
-                    blockquote = false;
-                }
-                temp_s.push_str(&format!("</h{}>\n", s_len));
-                
-                
-                tokens.push(temp_s);
-                // println!("{}", temp_s);
-            }else{
-                
-                let combined_contents = format!("{} {}", symbol, contents.trim());
-
-                
-                let contents2 = convert::converter(&combined_contents);
-                println!("{}",contents2);
-                    
-                tokens.push(format!("<p>{}</p>", &convert::converter(&combined_contents)) )
-                
-                // let full_contents = format!("<p>{} {}</p>\n", symbol, contents.trim());
-                
-                // if full_contents != "<p> </p>"{
-                //     tokens.push(full_contents);
-                // }
-            }
-        }
-        
-    }
-
-   for line in &tokens {
-       println!("{}", line);
-       // outfile.write_all(line.as_bytes())
-       //      .expect("[ ERROR ] Could not write to output file!");
-   }
-   println!("[ INFO ] Parsing complete!");
-}
-
-#[derive(Debug)]
-struct Parser {
-    symbol_stack: Vec<String>,
-    // symbol_stack: Vec<&'static str>,
-    // content_stack: Vec<&'static str>,
-    content_stack: Vec<String>,
-}
-
-
-fn parse_test(_filename: &str){
-
-
-    let input_filename = Path::new(_filename);
-    
-    let file = File::open(&input_filename)
-                .expect("[ ERROR ] Failed to open file!");
 
     let reader = BufReader::new(file);
     
@@ -143,19 +56,23 @@ fn parse_test(_filename: &str){
                 let top_len = top.len();
                 if let Some(converted_bold) = convert::bold(&top) {
                     p.content_stack.push(converted_bold);
-                }else{
-                    if keywords.contains(&top.as_str()){
-                        p.content_stack.insert(0, format!("</h{}>", top_len));
-                       p.content_stack.push(format!("<h{}>", top_len ));
-                    } else{
-                        p.content_stack.push(top.to_string())
-                    }
                 }
+                // else{
+                else if top == ">" {
+                    p.content_stack.push("<blockquote>".to_string());
+                    p.content_stack.insert(0,"</blockquote>".to_string());
+                }
+                else if keywords.contains(&top.as_str()){
+                    p.content_stack.insert(0, format!("</h{}>", top_len));
+                   p.content_stack.push(format!("<h{}>", top_len ));
+                } else{
+                    p.content_stack.push(top.to_string())
+                }
+                // }
             }
             
             p.content_stack.reverse();
             let first_item = p.content_stack[0].as_str();
-            utility::print_type_of(&first_item);
             
             if let Some(first_char) = first_item.get(0..1) {
                 if first_char != "<" {
@@ -164,44 +81,28 @@ fn parse_test(_filename: &str){
                 }
             }
                 
-            println!("{}",p.content_stack.join(" "));
+            tokens.push(format!("{}\n",p.content_stack.join(" ")) );
+            
+            for line in &tokens{
+                println!("{}", line);
+            }
         
         }
 
     }
+   println!("[ INFO ] Parsing complete!");
 }
 
 
 fn main() {
 
     let args: Vec<String> = std::env::args().collect();
-
+    
     match args.len(){
         1 => info::usage(),
-        2 => parse_test(&args[1]),
+        2 => parse_markdown_file(&args[1]),
         _ => {
-            println!("[ERROR] Invalid invocation (you done something wrong!)");
+            println!("[ERROR] Invalid invocation (you done goofed!)");
         }
     }
-    // parse_test()
-
-    //
-    //
-    //
-    // println!("{:?}",p.content_stack.iter().rev().next());
-
-    // let ss: Option<(&str, &str)> = s.split_once(' ');
-    
-    // if let Some((symbol, contents)) = ss {
-    //     println!("{}, {}", symbol, contents);
-    // }
-    
-    // let args: Vec<String> = std::env::args().collect();
-    // match args.len(){
-    //     1 => info::usage(),
-    //     2 => parse_markdown_file(&args[1]),
-    //     _ => {
-    //         println!("[ERROR] Invalid invocation (you done goofed!)");
-    //     }
-    // }
 }
